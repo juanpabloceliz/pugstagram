fab99f533e7860dd9fb5619096d2752cb6d10340
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-11-04 16:07:02 -0300

    trying fix the bug with compile onmount

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,11 +1,41 @@
+<script>
+  export let nickname;
+  export let name;
+</script>
+
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
 <div class="Profile">
-  <div class="Profile-container">
+  <div class="Profile-content">
     <div class="Profile-avatar">
-      <img src="https://imgur.com/nj9RFAs" alt="Profile picture" />
+      <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="" />
     </div>
     <div class="Profile-info">
-      <h2>juano1204</h2>
-      <span>Juan Pablo Celiz</span>
+      <h2>{nickname}</h2>
+      <span>{name}</span>
     </div>
   </div>
 </div>
